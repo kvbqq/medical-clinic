7e6df9ae05587618c0674d531f82ae2c5f3f857b
kvbqq <kvbqqq@gmail.com>
2025-04-15 20:01:39 +0200

    feat: Swagger

--- src/main/java/com/example/medical_clinic/controller/PatientController.java
+++ src/main/java/com/example/medical_clinic/controller/PatientController.java
@@ -5,7 +5,10 @@ import com.example.medical_clinic.model.ChangePassword;
 import com.example.medical_clinic.model.CreatePatientCommand;
 import com.example.medical_clinic.model.PatientDto;
 import com.example.medical_clinic.service.PatientService;
-import com.example.medical_clinic.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +17,16 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Patient", description = "Patients operations")
 @RequestMapping("/patients")
 public class PatientController {
     private final PatientService patientService;
     private final PatientMapStructMapper patientMapper;
 
+    @Operation(summary = "Get all Patients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patients found")
+    })
     @GetMapping
     public List<PatientDto> getAllPatients() {
         return patientService.getAllPatients().stream()
@@ -26,26 +34,51 @@ public class PatientController {
                 .collect(Collectors.toList());
     }
 
+    @Operation(summary = "Get Patient by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient found"),
+            @ApiResponse(responseCode = "404", description = "Patient with given email does not exist")
+    })
     @GetMapping("/{email}")
     public PatientDto getPatient(@PathVariable String email) {
         return patientMapper.toDto(patientService.getPatient(email));
     }
 
+    @Operation(summary = "Create new Patient")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Patient created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid Patient data")
+    })
     @PostMapping
     public PatientDto createPatient(@RequestBody CreatePatientCommand command) {
         return patientMapper.toDto(patientService.createPatient(patientMapper.toPatientEntity(command)));
     }
 
+    @Operation(summary = "Delete Patient by email")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Patient deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Patient with given email does not exist")
+    })
     @DeleteMapping("/{email}")
     public void removePatient(@PathVariable String email) {
         patientService.removePatient(email);
     }
 
+    @Operation(summary = "Update Patient data")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Patient updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Patient with given email does not exist")
+    })
     @PutMapping("/{email}")
     public PatientDto updatePatient(@PathVariable String email, @RequestBody CreatePatientCommand command) {
         return patientMapper.toDto(patientService.updatePatient(email, patientMapper.toPatientEntity(command)));
     }
 
+    @Operation(summary = "Change Patient's password")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Password changed successfully"),
+            @ApiResponse(responseCode = "404", description = "Patient with given email does not exist")
+    })
     @PatchMapping("/{email}/password")
     public PatientDto changePatientPassword(@PathVariable String email, @RequestBody ChangePassword changePassword) {
         return patientMapper.toDto(patientService.changePatientPassword(email, changePassword));
--- src/main/java/com/example/medical_clinic/controller/UserController.java
+++ src/main/java/com/example/medical_clinic/controller/UserController.java
@@ -4,6 +4,10 @@ import com.example.medical_clinic.mapper.UserMapper;
 import com.example.medical_clinic.model.CreateUserCommand;
 import com.example.medical_clinic.model.UserDto;
 import com.example.medical_clinic.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +17,16 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "User", description = "Users operations")
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
+    @Operation(summary = "Get all users")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Users found")
+    })
     @GetMapping
     public List<UserDto> getAllUsers() {
         return userService.getAllUsers().stream()
@@ -25,22 +34,42 @@ public class UserController {
                 .collect(Collectors.toList());
     }
 
+    @Operation(summary = "Get User by username")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "404", description = "User with given username does not exist")
+    })
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @GetMapping("/{username}")
     public UserDto getUser(@PathVariable String username) {
         return userMapper.toDto(userService.getUser(username));
     }
 
+    @Operation(summary = "Create new User")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid User data")
+    })
     @PostMapping
     public UserDto createUser(@RequestBody CreateUserCommand command) {
         return userMapper.toDto(userService.createUser(userMapper.toUserEntity(command)));
     }
 
+    @Operation(summary = "Delete a user by username")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User with given username does not exist")
+    })
     @DeleteMapping("/{username}")
     public void removeUser(@PathVariable String username) {
         userService.removeUser(username);
     }
 
+    @Operation(summary = "Update User data")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User updated successfully"),
+            @ApiResponse(responseCode = "404", description = "User with given username does not exist")
+    })
     @PutMapping("/{username}")
     public UserDto updateUser(@PathVariable String username, @RequestBody CreateUserCommand command) {
         return userMapper.toDto(userService.updateUser(username, userMapper.toUserEntity(command)));
